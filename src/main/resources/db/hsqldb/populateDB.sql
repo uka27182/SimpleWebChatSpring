3fb2e137eb8c3ce560b704f7dae1078aa21cf1ea
uka27182 <uka000@rambler.ru>
2015-04-23 10:07:06 +0300

    Update populateDB.sql
    change test data

--- src/main/resources/db/hsqldb/populateDB.sql
+++ src/main/resources/db/hsqldb/populateDB.sql
@@ -1,12 +1,12 @@
 -- TRUNCATE TABLE chat_history;
 
-INSERT INTO chat_history VALUES (1, '2015-04-20 10:00:00', 'Carter', 'message 1');
-INSERT INTO chat_history VALUES (2, '2015-04-20 11:00:00', 'Linda', 'message 2');
-INSERT INTO chat_history VALUES (3, '2015-04-20 12:00:00', 'Carter', 'message 3');
-INSERT INTO chat_history VALUES (4, '2015-04-20 13:00:00', 'Rafael', 'message 4');
-INSERT INTO chat_history VALUES (5, '2015-04-21 10:00:00', 'Carter', 'message 5');
-INSERT INTO chat_history VALUES (6, '2015-04-21 11:00:00', 'Henry', 'message 6');
-INSERT INTO chat_history VALUES (7, '2015-04-21 10:00:00', 'Carter', 'message 7');
-INSERT INTO chat_history VALUES (8, '2015-04-22 09:00:00', 'Helen', 'message 8');
-INSERT INTO chat_history VALUES (9, '2015-04-22 12:00:00', 'Sharon', 'message 9');
-INSERT INTO chat_history VALUES (10, '2015-04-22 12:10:00', 'Carter', 'message 10');
+INSERT INTO chat_history VALUES (1, '2015-04-20 10:00', 'Carter', 'message 1');
+INSERT INTO chat_history VALUES (2, '2015-04-20 11:00', 'Linda', 'message 2');
+INSERT INTO chat_history VALUES (3, '2015-04-20 12:00', 'Carter', 'message 3');
+INSERT INTO chat_history VALUES (4, '2015-04-20 13:00', 'Rafael', 'message 4');
+INSERT INTO chat_history VALUES (5, '2015-04-21 10:00', 'Carter', 'message 5');
+INSERT INTO chat_history VALUES (6, '2015-04-21 11:00', 'Henry', 'message 6');
+INSERT INTO chat_history VALUES (7, '2015-04-21 10:00', 'Carter', 'message 7');
+INSERT INTO chat_history VALUES (8, '2015-04-22 13:00', 'Helen', 'message 8');
+INSERT INTO chat_history VALUES (9, '2015-04-22 22:00', 'Sharon', 'message 9');
+INSERT INTO chat_history VALUES (10, '2015-04-22 22:10', 'Carter', 'message 10');
